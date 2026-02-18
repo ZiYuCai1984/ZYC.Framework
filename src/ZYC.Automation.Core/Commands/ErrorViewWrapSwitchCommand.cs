cfccc526e37697252dbbdbc177d4bfa5801b5976
ZiYuCai1984 <64200196+ZiYuCai1984@users.noreply.github.com>
2026-02-18 16:52:24 +0900

    Features/support wrap in error view (#23)
    * Refactored Chronosynchronicity module: updated namespace, switched constants to const, and referenced Abstractions project. Improved HybridIcon handling for empty names. Updated CLIView.Dispose for safety. Refactored ChronosynchronicityTabItemFactory to use TabItemFactoryBase and TabItemRoute.
    
    * add wrap support to the error view
    
    ---------
    
    Co-authored-by: _ <_@_.com>

---
+++ src/ZYC.Automation.Core/Commands/ErrorViewWrapSwitchCommand.cs
@@ -0,0 +1,44 @@
+﻿using ZYC.Automation.Abstractions.State;
+using ZYC.CoreToolkit.Extensions.Autofac.Attributes;
+
+namespace ZYC.Automation.Core.Commands;
+
+[RegisterSingleInstance]
+public class ErrorViewWrapSwitchCommand : CommandBase
+{
+    public ErrorViewWrapSwitchCommand(ErrorViewState errorViewState)
+    {
+        ErrorViewState = errorViewState;
+    }
+
+    private ErrorViewState ErrorViewState { get; }
+
+
+    public string Text
+    {
+        get
+        {
+            if (ErrorViewState.IsTextWrap)
+            {
+                return "Disable Word Wrap";
+            }
+
+            return "Enable Word Wrap";
+        }
+    }
+
+
+    public bool IsTextWrap
+    {
+        get => ErrorViewState.IsTextWrap;
+        set => ErrorViewState.IsTextWrap = value;
+    }
+
+    protected override void InternalExecute(object? parameter)
+    {
+        IsTextWrap = !IsTextWrap;
+
+        OnPropertyChanged(nameof(IsTextWrap));
+        OnPropertyChanged(nameof(Text));
+    }
+}
