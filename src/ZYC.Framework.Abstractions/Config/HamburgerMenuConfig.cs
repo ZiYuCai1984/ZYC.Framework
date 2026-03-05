752480ceff3c2691ee577a19ee51146d78d6c7e4
_ <_@_.com>
2026-03-06 03:00:19 +0900

    Add Hamburger Menu system and refactor MainMenu logic
    Introduce HamburgerMenuConfig, IHamburgerMenuManager, and HamburgerMenuManager for new hamburger menu functionality. Refactor MainMenuView to support both menus with updated layout and styles. Improve error handling in CycleWebViewHostBase, update localization resources, and enhance documentation and build config.

---
+++ src/ZYC.Framework.Abstractions/Config/HamburgerMenuConfig.cs
@@ -0,0 +1,17 @@
+﻿using PropertyChanged;
+using ZYC.CoreToolkit.Abstractions.Settings;
+
+namespace ZYC.Framework.Abstractions.Config;
+
+/// <summary>
+///     Configuration settings for the hamburger menu component.
+///     Implements the <see cref="IConfig" /> interface.
+/// </summary>
+[AddINotifyPropertyChangedInterface]
+public class HamburgerMenuConfig : IConfig
+{
+    /// <summary>
+    ///     Gets or sets a value indicating whether the hamburger menu is visible in the user interface.
+    /// </summary>
+    public bool IsVisible { get; set; } = false;
+}
--- src/ZYC.Framework.Abstractions/UriQueryNameAttribute.cs
+++ src/ZYC.Framework.Abstractions/UriQueryNameAttribute.cs
@@ -1,12 +1,22 @@
 ﻿namespace ZYC.Framework.Abstractions;
 
+/// <summary>
+///     Specifies the custom name for a URI query parameter associated with a property or parameter.
+/// </summary>
 [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property)]
 public sealed class UriQueryNameAttribute : Attribute
 {
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="UriQueryNameAttribute" /> class.
+    /// </summary>
+    /// <param name="name">The name of the query string parameter.</param>
     public UriQueryNameAttribute(string name)
     {
         Name = name;
     }
 
+    /// <summary>
+    ///     Gets the custom name of the URI query parameter.
+    /// </summary>
     public string Name { get; }
 }
