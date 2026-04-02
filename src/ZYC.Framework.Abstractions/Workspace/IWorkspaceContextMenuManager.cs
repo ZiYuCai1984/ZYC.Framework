a07bc414f03a5953e871a8c985258743be0f14b6
_ <_@_.com>
2026-04-02 20:23:34 +0900

    add WorkspaceContextMenuManager

--- src/ZYC.Framework.Abstractions/Workspace/IWorkspaceContextMenuManager.cs
+++ src/ZYC.Framework.Abstractions/Workspace/IWorkspaceContextMenuManager.cs
@@ -1,6 +1,11 @@
 ﻿namespace ZYC.Framework.Abstractions.Workspace;
 
-internal interface IWorkspaceContextMenuManager
+/// <summary>
+///     Defines a specialized manager for workspace-related context menu items.
+///     Inherits from <see cref="IMenuManager{T}" /> to provide standard lifecycle
+///     and collection management for <see cref="IWorkspaceMenuItem" /> objects.
+/// </summary>
+public interface IWorkspaceContextMenuManager : IMenuManager<IWorkspaceMenuItem>
 {
-    //TODO-zyc IWorkspaceContextMenuManager
+
 }
