a4a473a3786de63d3c746c0dcdffd2facb481535
ZiYuCai1984 <64200196+ZiYuCai1984@users.noreply.github.com>
2026-07-31 14:19:58 +0900

    - Replaced command bindings with Click event in AspireServiceStartSuccessToastView. (#86)
    - Updated AspireServiceStartSuccessToastView.xaml.cs to use NavigateCommand in event handler.
    
    Co-authored-by: _ <_@_.com>

--- src/ZYC.Framework.Modules.Aspire/UI/Toast/AspireServiceStartSuccessToastView.xaml.cs
+++ src/ZYC.Framework.Modules.Aspire/UI/Toast/AspireServiceStartSuccessToastView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using ZYC.CoreToolkit.Extensions.Autofac.Attributes;
+﻿using System.Windows;
+using ZYC.CoreToolkit.Extensions.Autofac.Attributes;
+using ZYC.Framework.Core.Commands;
 using ZYC.Framework.Modules.Aspire.Abstractions;
 
 namespace ZYC.Framework.Modules.Aspire.UI.Toast;
@@ -6,11 +8,19 @@ namespace ZYC.Framework.Modules.Aspire.UI.Toast;
 [Register]
 internal partial class AspireServiceStartSuccessToastView
 {
-    public AspireServiceStartSuccessToastView()
+    public AspireServiceStartSuccessToastView(NavigateCommand navigateCommand)
     {
+        NavigateCommand = navigateCommand;
         InitializeComponent();
     }
 
-
     public Uri TargetUri => AspireModuleContansts.Uri;
+
+    private NavigateCommand NavigateCommand { get; }
+
+    private void OnNavigateButtonClick(object sender, RoutedEventArgs e)
+    {
+        NavigateCommand.Execute(TargetUri);
+        CloseNotificationCommand.Execute(null);
+    }
 }
